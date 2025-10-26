8f7dacc7ddc6f0248b549da1546b476dd17d2c03
Luciano Ciccariello <xeeynamo@hotmail.com>
2025-10-26 15:07:59 +0000

    Allow to differ overlays

--- diff_settings.py
+++ diff_settings.py
@@ -1,22 +1,19 @@
 #!/usr/bin/env python3
 
-import os
-import re
-
 
 def add_custom_arguments(parser):
     parser.add_argument(
         "--overlay",
-        default="psxus",
+        default="main",
         dest="overlay",
     )
 
 
 def apply(config, args):
     config["baseimg"] = "disks/us/SCUS_941.63"
-    config["myimg"] = "build/us/main.bin"
-    config["mapfile"] = "build/us/main.map"
-    config["source_directories"] = ["src/psxus"]
+    config["myimg"] = f"build/us/{args.overlay}.bin"
+    config["mapfile"] = f"build/us/{args.overlay}.map"
+    config["source_directories"] = [f"src/{args.overlay}"]
     config["build_dir"] = "build/"
     config["expected_dir"] = "expected/"
     config["objdump_executable"] = "mipsel-linux-gnu-objdump"
